d51afb3e14b094845d0488ff888b97eec866e1f1
Ray Dennis <raydennis@outlook.com>
2019-09-06 16:05:14 -0600

    cleaned up runme.bash

--- runme.bash
+++ runme.bash
@@ -1,24 +1,12 @@
 echo "get versions"
 make --version
-python --version
-
-echo "get folders"
-pwd
-ls -lha
-echo ".docker"
-ls -lha .docker
-echo "/python"
-ls -lha system-tests/selenium/examples/python 
+python3 --version
 
 echo "build containers"
-make help
 make docker-build
-make docker-test
 
 echo "setup python environment"
-cd system-tests/selenium/examples/python
-
 pip3 install selenium==3.141.0
 
 echo "run python example test"
-python3 selenium-google-test.py
+python3 system-tests/selenium/examples/python/selenium-google-test.py
